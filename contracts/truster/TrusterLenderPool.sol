91c5d584f840a25d6fe898b628050f14e688cf29
nizhunt <nizhunt.eth@gmail.com>
2022-11-26 12:39:14 +0530

    first commit

---
+++ contracts/truster/TrusterLenderPool.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+// @audit-ok
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+/**
+ * @title TrusterLenderPool
+ * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
+ */
+contract TrusterLenderPool is ReentrancyGuard {
+
+    using Address for address;
+
+    IERC20 public immutable damnValuableToken;
+
+    constructor (address tokenAddress) {
+        damnValuableToken = IERC20(tokenAddress);
+    }
+
+    function flashLoan(
+        uint256 borrowAmount,
+        address borrower,
+        address target,
+        bytes calldata data
+    )
+        external
+        nonReentrant
+    {
+        uint256 balanceBefore = damnValuableToken.balanceOf(address(this));
+        require(balanceBefore >= borrowAmount, "Not enough tokens in pool");
+        
+        damnValuableToken.transfer(borrower, borrowAmount);  
+        target.functionCall(data);  
+        // @audit-info this lets the caller of this function make this contract call anyone and access any function
+
+        uint256 balanceAfter = damnValuableToken.balanceOf(address(this));
+        require(balanceAfter >= balanceBefore, "Flash loan hasn't been paid back");
+    }
+
+}
